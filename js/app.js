8840df1aebdbbc995ace518afc3e6b6478c890d8
Andres Sevillano Molina <andres.sevillano.net@gmail.com>
2022-06-24 21:48:59 -0500

    Agregado un nuevo card similar al anterior pero con otra imagen.

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ date = new Date();
 year = date.getFullYear();
 month = date.getMonth() + 1;
 day = date.getDate();
-document.getElementById("current_date").innerHTML ="Última modificación: " + day + "/" + month + "/" + year + ".";
+// Add year month and day to all the elements with the class "date"
+document.querySelectorAll('.current_date').forEach(function(element) {
+    element.innerHTML ="Ùltima modificaciòn: " + day + "/" + month + "/" + year;
+}
+);
