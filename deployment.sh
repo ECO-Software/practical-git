b6c7a50196f894d2903f17538e716101448dc942
Paola Erazo <paolagabrirlaerazovelastegui@gmail.com>
2022-06-26 15:18:39 -0500

    Testing bash on proyect for deploy page 5

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 git push -u origin master
-ssh root@192.168.1.250 cd ../var/www/html/course-git
-git pull origin master 
-sudo systemctl restart apache2
+ssh root@192.168.1.250 bash deployment.sh
